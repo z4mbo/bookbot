013985a6eb2f564842eeddd28058d7cf46219401
AleZambon97 <alessandrozambon1997@gmail.com>
2025-03-28 00:42:20 +0100

    dynamic books

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_num_words,
     chars_dict_to_sorted_list,
@@ -6,7 +7,11 @@ from stats import (
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -34,5 +39,3 @@ def print_report(book_path, num_words, chars_sorted_list):
 
 
 main()
-
-
